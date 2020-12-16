c63a9c2e44dc0366253f2dfca5c9aac92b575c9b
Katie Poa <kpoa@berkeley.edu>
2020-12-15 23:04:09 -0800

    Update Contains_Duplicate.js

--- Contains_Duplicate.js
+++ Contains_Duplicate.js
@@ -1,3 +1,13 @@
+# Python
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                return True
+            dict[nums[i]] = i
+        return False
+
 /**
  * @param {number[]} nums
  * @return {boolean}
