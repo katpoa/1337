0501b8bb327b92a14ee97b02406a53f49b6b8a32
Katie Poa <kpoa@berkeley.edu>
2020-12-30 17:27:24 -0800

    Update Maximum Subarray.js

--- Maximum Subarray.js
+++ Maximum Subarray.js
@@ -3,28 +3,39 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum;
+    let maxSum = nums[0];
+    let currSum = 0;
     for (let i = 0; i < nums.length; i++) {
-        let sum = 0;
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j];
-            if (maxSum === undefined || sum > maxSum) {
-                maxSum = sum;
-            }
+        if (currSum < 0) {
+            currSum = 0;
+        }
+        currSum += nums[i];
+        if (currSum > maxSum) {
+            maxSum = currSum;
         }
     }
     return maxSum;
+//     O(n**2) time solution
+//     let maxSum;
+//     for (let i = 0; i < nums.length; i++) {
+//         let sum = 0;
+//         for (let j = i; j < nums.length; j++) {
+//             sum += nums[j];
+//             if (maxSum === undefined || sum > maxSum) {
+//                 maxSum = sum;
+//             }
+//         }
+//     }
+//     return maxSum;
 };
 
 # python solution
-#***TIMES OUT
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
-        for i in range(len(nums)):
-            currSum = 0
-            for j in range(i, len(nums)):
-                currSum += nums[j]
-                if (currSum > maxSum):
-                    maxSum = currSum
+        maxSum, currSum = nums[0], 0
+        for n in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += n
+            maxSum = max(maxSum, currSum)
         return maxSum
