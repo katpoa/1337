69fe661557865aacb722296a6a333c71c1b4a7a4
Katie Poa <kpoa@berkeley.edu>
2021-01-07 00:45:19 -0800

    Update Find Minimum in Rotated Sorted Array.js

--- Find Minimum in Rotated Sorted Array.js
+++ Find Minimum in Rotated Sorted Array.js
@@ -1,3 +1,51 @@
+// Binary Search sol'n
+// Javascript
+
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    if (nums.length === 1) {
+        return nums[0];
+    }
+    if (nums[right] > nums[left]) {
+        return nums[0];
+    }
+    while (right > left) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            return nums[mid + 1];
+        }
+        if (nums[mid] < nums[mid - 1]) {
+            return nums[mid];
+        }
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+};
+
+# Python
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        left = 0
+        right = len(nums) - 1
+        if nums[right] > nums[left]:
+            return nums[0]
+        while right >= left:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid] < nums[mid - 1]:
+                return nums[mid]
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
 var findMin = function(nums) {
 //     find the largest difference?
     let diff = nums[nums.length - 1] - nums[0];
