1339c9ecd9576d011572ce635fae457663d26702
Katie Poa <kpoa@berkeley.edu>
2021-01-22 16:40:58 -0800

    Create maxSubarraySum.js

---
+++ Sliding Window (problem type)/maxSubarraySum.js
@@ -0,0 +1,38 @@
+function maxSubarraySum(arr, num) {
+  // sliding windoow
+  if (num > arr.length) {
+    return null;
+  } 
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let sum = 0;
+    for (let j = 0; j < num; j++) {
+      sum += arr[i + j];
+    }
+    if (sum > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  let maxSum = 0;
+  let currSum = 0;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  currSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    currSum -= arr[i - num];
+    currSum += arr[i];
+    if (currSum > maxSum) {
+      maxSum = currSum;
+    }
+    // maxSum = Math.max(maxSum, currSum);
+  }
+  return maxSum;
+}
