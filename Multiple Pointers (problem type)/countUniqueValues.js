17161c599679111d604eb5b758ffbbdaeb255c49
Katie Poa <kpoa@berkeley.edu>
2021-02-02 14:11:58 -0800

    Update countUniqueValues.js

--- Multiple Pointers (problem type)/countUniqueValues.js
+++ Multiple Pointers (problem type)/countUniqueValues.js
@@ -1,3 +1,6 @@
+// Implement a function which accepts a sorted array and counts the unique values in the array
+// (there can be negative numbers, but it will always be sorted)
+
 function countUniqueValues(nums) {
   let left = 0;
   if (nums.length === 0) {
