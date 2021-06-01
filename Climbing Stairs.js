2e855cfd6df7db590637bdeb4de59aec81fd3255
Katie Poa <kpoa@berkeley.edu>
2021-06-01 03:15:28 -0700

    Update Climbing Stairs.js

--- Climbing Stairs.js
+++ Climbing Stairs.js
@@ -2,6 +2,7 @@
  * @param {number} n
  * @return {number}
  */
+// Recursive
 var climbStairs = function(n) {
     let counter = 0;
     if (n === 1) {
@@ -13,4 +14,16 @@ var climbStairs = function(n) {
     counter += climbStairs(n - 1);
     counter += climbStairs(n - 2);
     return counter;
+};
+
+// Iterative dp
+
+var climbStairs = function(n) {
+    let ways = [];
+    ways[1] = 1;
+    ways[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        ways[i] = ways[i - 1] + ways[i - 2];
+    }
+    return ways[n];
 };
